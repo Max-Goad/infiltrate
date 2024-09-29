fd861011522877bc93f867d3c69b5a7a1195464a
Max Goad <maxlgoad@gmail.com>
2024-09-29 22:14:25 +0900

    Messed around with lighting (including adding other light sources)

--- scripts/visibility_light.gd
+++ scripts/visibility_light.gd
@@ -6,5 +6,6 @@ func update_position(target_position: Vector2) -> void:
 	match component.relativity:
 		LightComponent.Relativity.WORLD_RELATIVE:
 			self.global_position = target_position
+			print(self.global_position)
 		LightComponent.Relativity.CAMERA_RELATIVE:
 			self.global_position = target_position + component.global_position
