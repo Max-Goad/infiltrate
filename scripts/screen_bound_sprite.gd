65a7c75d56fe1fe926b99ba1dc3e67e5e93de2f3
Max Goad <maxlgoad@gmail.com>
2024-10-03 16:45:07 +0900

    Refactored ScreenBoundSprite to use bound/vector intersection to determine position

--- scripts/screen_bound_sprite.gd
+++ scripts/screen_bound_sprite.gd
@@ -24,13 +24,13 @@ func _process(_delta: float) -> void:
 	const buffer = 20
 	var bounds := camera.get_viewport_rect().grow(-buffer)
 	bounds.position = camera.bounded_position() + Vector2(buffer, buffer)
-	var on_screen = bounds.has_point(reference.global_position)
-	if on_screen:
-		self.hide()
-	else:
-		global_position = reference.global_position.clamp(bounds.position, bounds.end)
+	var intersection = Math.segment_intersects_rect(camera.get_screen_center_position(), reference.global_position, bounds)
+	if intersection:
+		global_position = intersection
 		global_scale = _get_scale_by_distance(reference.global_position)
 		self.show()
+	else:
+		self.hide()
 #endregion
 
 #region Public Functions
@@ -41,6 +41,5 @@ func _get_scale_by_distance(target: Vector2) -> Vector2:
 	var distance = camera.get_screen_center_position().distance_to(target)
 	var percent = clamp((distance - close_distance) / (far_distance - close_distance), 0.0, 1.0)
 	var new_scale = (max_size - min_size) * (1.0-percent) + min_size
-	print({"distance" : distance, "percent" : percent, "new_scale" : new_scale})
 	return Vector2(new_scale, new_scale)
 #endregion
