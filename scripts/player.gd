1e5b9c9df8b7907a1c696e46c75ff40c63f09e09
Max Goad <maxlgoad@gmail.com>
2024-10-03 21:21:46 +0900

    Added dino sprite to player

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,9 @@ class_name Player extends CharacterBody2D
 
 #region Variables
 @onready var movement: CharacterMovementComponent = $CharacterMovementComponent
+@onready var sprite: AnimatedSprite2D = $Sprite
 
-@export var followers: Array[Node2D] = []
+var last_movement_direction := Vector2.RIGHT
 #endregion
 
 #region Signals
@@ -14,9 +15,8 @@ func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
-	for follower in followers:
-		follower.global_position = self.global_position
 	_process_acceleration()
+	_process_animation()
 	_process_deceleration()
 #endregion
 
@@ -27,7 +27,19 @@ func _process(_delta: float) -> void:
 func _process_acceleration():
 	var direction := Input.get_vector("player_left", "player_right", "player_up", "player_down")
 	movement.apply_velocity(direction * movement.top_speed)
+	if direction != Vector2.ZERO:
+		last_movement_direction = direction
 
 func _process_deceleration():
 	movement.decelerate()
+
+func _process_animation():
+	if movement.moving():
+		sprite.play("run")
+	else:
+		sprite.play("idle")
+	if last_movement_direction.x > 0:
+		sprite.flip_h = false
+	elif last_movement_direction.x < 0:
+		sprite.flip_h = true
 #endregion
